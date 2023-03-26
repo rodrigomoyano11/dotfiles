50f0019efb6a9aff700997a353e9702bc6e90ddf
Rodrigo Moyano <rodrigomoyano11@gmail.com>
2023-03-26 02:41:12 -0300

    🧠 Update dotfiles

--- install.sh
+++ install.sh
@@ -1,13 +1,24 @@
 #!bin/bash
 
+# Upgrade packages
+# apt-get -y update
+# apt-get -y upgrade
+
 # Install zsh
 apt-get install -y zsh
 
 # Install packages
 apt-get install -y curl git openssh-client
 
+# Install Node.js
+curl -fsSL https://deb.nodesource.com/setup_lts.x | bash - && apt-get install -y nodejs
+
 # Change default shell to zsh
 chsh --shell $(which zsh)
+
+# Install Node.js packages
+npm install -g npm pnpm
+npm i -g @githubnext/github-copilot-cli
 
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
@@ -16,17 +27,6 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/jirutka/zsh-shift-select ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-shift-select
-
-# Upgrade packages
-# apt-get -y update
-# apt-get -y upgrade
-
-# Install Node.js
-curl -fsSL https://deb.nodesource.com/setup_lts.x | bash - && apt-get install -y nodejs
-
-# Install Node.js packages
-npm install -g npm pnpm
-npm i -g @githubnext/github-copilot-cli
 
 # Add dotfiles
 curl -fsSL https://raw.githubusercontent.com/rodrigomoyano11/dotfiles/main/.zshrc >~/.zshrc
